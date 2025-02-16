0e049b66769e16209f809a1a9bce9bcd8efaf2e1
shanjidarahaman <shanjidarahaman40@gmail.com>
2025-02-16 21:37:07 +0100

    exercise

---
+++ JS.Conditionals/exercise.js
@@ -0,0 +1,21 @@
+// const x = ""
+// if(!x.length){
+// console.log("Hey")
+// }else{
+// console.log("Hi")
+// }
+// let str = "Mississippi";
+// console.log(str.indexOf("i", 3));
+// const myObject = {
+//     a: 1,
+//     b: 2,
+//     c: 3
+//    };
+   
+//    for (let prop in myObject) {
+//     console.log(myObject[prop]);
+//    }
+   const str = "Hello";
+str[0] = "h";
+console.log(str);
+console.log
---
+++ JS.Conditionals/stepped.js
@@ -0,0 +1,38 @@
+// const price = 4000;
+
+// if (price >= 5000){
+//     const discount = price * 10/100;
+//     const payAmount = price - discount ;
+//     console.log(payAmount);
+// }
+// else if( price > 2500){
+//     const discount = price * 5/100;
+//     const payAmount = price - discount;
+//     console.log(payAmount);
+
+// }
+// else {
+//     console.log(price);
+// }
+
+const money = 500;
+if (money > 600){
+    console.log('ami wings khabo');
+}
+else if( money > 300){
+    
+     if(money > 100){
+        console.log('ami pani khabo')
+     }
+    else if (money > 50){
+        console.log('ami adha gorib')
+    }
+    else {
+        console.log('ami bfc khabo')
+    }
+    
+
+}
+else {
+    console.log('ami gorib')
+}
