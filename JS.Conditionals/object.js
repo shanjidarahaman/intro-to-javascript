9e670319d9f30dfa4064a359458400400ba29a6a
shanjidarahaman <shanjidarahaman40@gmail.com>
2025-02-16 21:36:53 +0100

    object

---
+++ JS.Conditionals/object.js
@@ -0,0 +1,26 @@
+const age = 21;
+const school = 'rifles square';
+const isPassed = true;
+let isDeveloper;
+
+const subjects = ['bangla', 'English', 'Physics', 'Math'];
+const bottle = ['white', 45]
+const bottle = {
+    brand: 'apple',
+    price : 45,
+    color: 'white'
+    isClean: false
+}
+
+const subject = {
+    name: 'biology',
+    teacher: 'rasheda mam',
+    examDate: '30 feb',
+    chapters: ['first', 'second','third'],
+    exams: {
+        midterm: 90,
+        final: 85,
+        practice: 95
+    }
+}
+console.log(isDeveloper);
